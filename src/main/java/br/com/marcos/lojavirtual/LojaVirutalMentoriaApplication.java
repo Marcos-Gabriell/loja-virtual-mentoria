03dccd42615ee9d93664d83c3f3cdf1fee2b83ba
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-11-27 14:54:22 -0300

    Teste

--- src/main/java/br/com/marcos/lojavirtual/LojaVirutalMentoriaApplication.java
+++ src/main/java/br/com/marcos/lojavirtual/LojaVirutalMentoriaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LojaVirutalMentoriaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LojaVirutalMentoriaApplication.class, args);
+		System.out.println( "Hello World!" );
 	}
 
 }
